5422d85a468dde5f2e9242f6b7462575f7438ac8
truongvq <truongvq@scuti.asia>
2025-02-17 16:59:46 +0700

    change old

--- src/components/ecommerce/EcommerceMetrics.vue
+++ src/components/ecommerce/EcommerceMetrics.vue
@@ -25,7 +25,7 @@
 
       <div class="flex items-end justify-between mt-5">
         <div>
-          <span class="text-sm text-gray-500 dark:text-gray-400">Customers new</span>
+          <span class="text-sm text-gray-500 dark:text-gray-400">Customers</span>
           <h4 class="mt-2 font-bold text-gray-800 text-title-sm dark:text-white/90">3,782</h4>
         </div>
 
